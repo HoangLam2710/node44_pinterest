fb114f12901c8e67479e1352174419d41b3f365e
Lam Le <lam.le@swagsoft.com>
2024-11-03 23:18:28 +0700

    feat: add image upload and post creation functionality with validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
+
 import { decodeToken } from "../config/jwt.js";
 import catchAsync from "../utils/catch_async.js";
 import { omitUser, responseDataSuccess } from "../utils/user.js";
@@ -6,7 +8,7 @@ import { OK } from "../constant/error_code.js";
 
 const prisma = new PrismaClient();
 
-const uploadAvatar = catchAsync(async (req, res) => {
+const uploadAvatar = catchAsync(async (req, res, next) => {
   const { authorization } = req.headers;
   const userId = decodeToken(authorization);
   const { path: avatarPath } = req.file;
@@ -58,4 +60,42 @@ const updateAccount = catchAsync(async (req, res, next) => {
   responseDataSuccess(res, OK, "Update user successfully", omitUser(userNew));
 });
 
-export { uploadAvatar, updateAccount };
+const uploadImage = catchAsync(async (req, res, next) => {
+  const { path: imagePath } = req.file;
+
+  return res.status(OK).json({
+    message: "Upload image successfully!",
+    data: { path: imagePath },
+  });
+});
+
+const createPost = catchAsync(async (req, res, next) => {
+  const { authorization } = req.headers;
+  const userId = decodeToken(authorization);
+  const { imageName, imageUrl, description, additionalWebsite } = req.body;
+
+  const checkUser = await prisma.users.findFirst({
+    where: { user_id: userId },
+  });
+  if (!checkUser) {
+    return next(new AppError("User not found", NOT_FOUND));
+  }
+
+  const newPost = await prisma.images.create({
+    data: {
+      img_id: uuidv4(),
+      img_name: imageName,
+      img_url: imageUrl,
+      description,
+      additional_website: additionalWebsite,
+      user_id: userId,
+    },
+  });
+
+  return res.status(OK).json({
+    message: "Create post successfully!",
+    data: newPost,
+  });
+});
+
+export { uploadAvatar, updateAccount, uploadImage, createPost };
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { updateAccount, uploadAvatar } from "../controllers/user.controller.js";
+import {
+  createPost,
+  updateAccount,
+  uploadAvatar,
+  uploadImage,
+} from "../controllers/user.controller.js";
 import { middlewareToken } from "../config/jwt.js";
-import { updateAccountValidation } from "../middlewares/validations/user.validation.js";
+import {
+  createPostValidation,
+  updateAccountValidation,
+} from "../middlewares/validations/user.validation.js";
 import uploadCloud from "../config/upload_cloud.js";
 
 const userRoutes = express.Router();
@@ -12,14 +20,23 @@ userRoutes.post(
   uploadCloud("avatar").single("avatar"),
   uploadAvatar,
 );
-
-// uploadCloud.array('avatar', 10)
-
 userRoutes.put(
   "/update-account",
   middlewareToken,
   updateAccountValidation,
   updateAccount,
+);
+userRoutes.post(
+  "/upload-image",
+  middlewareToken,
+  uploadCloud("images").single("image"),
+  uploadImage,
+);
+userRoutes.post(
+  "/create-post",
+  middlewareToken,
+  createPostValidation,
+  createPost,
 );
 
 export default userRoutes;
